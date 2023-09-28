da93472f269d7be37c63d6057f192c35da01df30
Julien Erard <jerard@newrelic.com>
2023-09-28 16:48:02 -0700

    feat: add cli install implementation

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM ghcr.io/julien4218/ansible-runner:main
 
+RUN /root/.local/bin/ansible-galaxy collection install ansible.windows ansible.utils
+RUN /root/.local/bin/ansible-galaxy install newrelic.newrelic_install
+
 # Copies your code file from your action repository to the filesystem path `/` of the container
 # COPY entrypoint.sh /entrypoint.sh
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,47 @@
 #!/bin/sh
 
-result=$(python3 --version)
-result="$result\n"$(/root/.local/bin/ansible --version)
+python3 --version
+/root/.local/bin/ansible --version
 
+rm -f hosts
+export IFS=","
+for item in $NEW_RELIC_INVENTORY; do
+    echo "$item ansible_connection=ssh ansible_user=\"$NEW_RELIC_SSH_USER\" ansible_ssh_private_key_file=\"$NEW_RELIC_SSH_KEY_FILE\"" | tee -a hosts > /dev/null
+done
 
-echo "$result"
+rm -f playbook.yaml
+tee -a playbook.yaml > /dev/null <<"EOT"
+- name: Install New Relic
+  hosts: all
+  roles:
+    - role: newrelic.newrelic_install
+      vars:
+EOT
 
-success=$(echo "$result" | grep SUCCESS | wc -l)
-echo "success=$success" >> $GITHUB_ENV
-exit 0
+echo "        targets:" | tee -a playbook.yaml > /dev/null
+export IFS=","
+for item in $NEW_RELIC_INSTALL_TARGETS; do
+    echo "          - $item" | tee -a playbook.yaml > /dev/null
+done
+
+echo "  environment:" | tee -a playbook.yaml > /dev/null
+export IFS=","
+for item in $NEW_RELIC_INSTALL_ENVIRONMENT; do
+    echo "    $item" | tee -a playbook.yaml > /dev/null
+done
+echo "    NEW_RELIC_API_KEY: $NEW_RELIC_API_KEY" | tee -a playbook.yaml > /dev/null
+echo "    NEW_RELIC_ACCOUNT_ID: $NEW_RELIC_ACCOUNT_ID" | tee -a playbook.yaml > /dev/null
+echo "    NEW_RELIC_REGION: $NEW_RELIC_REGION" | tee -a playbook.yaml > /dev/null
+
+# debug
+# cat hosts
+# cat playbook.yaml
+
+ANSIBLE_HOST_KEY_CHECKING=$NEW_RELIC_SSH_KNOWN_HOSTS_CHECK_ENABLE /root/.local/bin/ansible-playbook -i hosts playbook.yaml
+exitStatus=$?
+
+if [ $exitStatus -ne 0 ]; then
+  echo "::error:: $result"
+fi
+
+exit $exitStatus
